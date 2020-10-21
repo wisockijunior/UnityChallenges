773dd725bebf2e6fe3d91b05e8eb5eaf3baaa2f6
wisockijunior <renato@wisockijunior.com.br>
2020-10-21 11:35:44 -0300

    main menu actions
    action for each button of main menu

---
+++ tier0/Assets/Scripts/MainMenuActions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnUserClick_NewGame()
+    {
+
+    }
+
+
+    public void OnUserClick_Normal()
+    {
+
+    }
+
+
+    public void OnUserClick_TimeTrial()
+    {
+
+    }
+
+
+    public void OnUserClick_Options()
+    {
+
+    }
+
+    public void OnUserClick_QuitGame()
+    {
+
+    }
+
+}
