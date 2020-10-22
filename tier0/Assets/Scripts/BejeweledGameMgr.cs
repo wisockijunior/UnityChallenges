8e649b03fb08e94a33c86522fbf31f03ffa4ef3b
wisockijunior <renato@wisockijunior.com.br>
2020-10-22 12:50:27 -0300

    Revert "initial game mgr"
    This reverts commit 0b460a4ae6ad67bf9e8785343eea75850b22e36d.

--- tier0/Assets/Scripts/BejeweledGameMgr.cs
+++ tier0/Assets/Scripts/BejeweledGameMgr.cs
@@ -1,32 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace SidiaUnityChallenge
+﻿public class BejeweledGameMgr
 {
-    public class BejeweledGameMgr
-    {
-
-        /// <summary>
-        /// reset the game, put the game in initial state
-        /// </summary>
-        public void StartNewGame()
-        { 
-        
-
-        }
-
-        
-        public void doit()
-        { 
-        
-        }
-
-
-        public void CheckForMouseClick()
-        {
-
-        }
-
-    }
 }
--- tier0/Assets/Scripts/MainMenuActions.cs
+++ tier0/Assets/Scripts/MainMenuActions.cs
@@ -1,16 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SidiaUnityChallenge;
 
 public class MainMenuActions : MonoBehaviour
 {
-    private void Awake()
-    {
-        if (GameMgr == null)
-            GameMgr = new BejeweledGameMgr();
-
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,20 +13,13 @@ public class MainMenuActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameMgr != null)
-        {
-            GameMgr.CheckForMouseClick();
-        }
+        
     }
-
-    
 
     public BejeweledGameMgr GameMgr;
 
     public void OnUserClick_NewGame()
     {
-        // new game
-        GameMgr.StartNewGame();
 
     }
 
